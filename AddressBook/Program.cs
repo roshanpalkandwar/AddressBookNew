19f26683c0be0533986871e62052d07592a6f7c1
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-10 19:40:50 +0530

    [Roshan]Added:Create Details Of User

---
+++ AddressBook/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter choice\n1.contact details");
+            int opt = Convert.ToInt32(Console.ReadLine());
+
+            switch (opt)
+            {
+                case 1:
+                    ContactDatails.Contact();
+
+
+                    break;
+            }
+        }
+    }
+}
