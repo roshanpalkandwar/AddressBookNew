e29bbf5202f33cdcdbf9e09763ba189cb7404423
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-15 21:37:04 +0530

    [Roshan]Refactor:add Multiple Address Book To The System

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter choice\n1.contact details\n2.add contacts dtails\n3.Edit contact\n4.Delete person contact\n5.add Multiple Person");
+            Console.WriteLine("enter choice\n1.contact details\n2.add contacts dtails\n3.Edit contact\n4.Delete person contact\n5.add Multiple Person\n6.Add multiper book address");
             int opt = Convert.ToInt32(Console.ReadLine());
 
             switch (opt)
@@ -23,6 +23,9 @@
                     break;
                 case 5:
                     AddMultPerson.AddBook5();
+                    break;
+                case 6:
+                    AddMultiyBook.AddBook6();
                     break;
             }
         }
